3fa358cce8e7d086def74622528e74ae178190d9
suzannelv <lvmeijuan9011@gmail.com>
2023-01-08 23:19:47 +0100

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Recipe} from '../recipe.model'
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[]=[
+    new Recipe('a test recipe', 'this simple test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'),
+    new Recipe('a test recipe', 'this simple test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg')
+  ];
+ constructor() {
+
+ }
+ ngOnInit(): void {
+  
+ }
+}
